9b32aca53f4d334a38772c105f4581496d2bea7c
davi <martinsoliveiradavi83@gmail.com>
2024-11-29 16:19:08 -0300

    alterando rotas para deploy

--- src/Routes/forbiddenError.jsx
+++ src/Routes/forbiddenError.jsx
@@ -4,7 +4,7 @@ const ForbiddenPage = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
       <div>403 Forbidden</div>
-      <Link to="https://davi0254.github.io/advanced-todo-list">Login</Link>
+      <Link to="/https://davi0254.github.io/advanced-todo-list">Login</Link>
     </div>
   );
 }
--- src/Routes/loginRoute.jsx
+++ src/Routes/loginRoute.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
  const loginRoute = () => {
     return (
       <nav style={{ marginTop: '9px' }}>
-        <Link to="https://davi0254.github.io/advanced-todo-list">sair</Link>
+        <Link to="/https://davi0254.github.io/advanced-todo-list">sair</Link>
       </nav>
     )
 }
