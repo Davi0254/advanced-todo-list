d85161abfadf683fd9a7ac842d7cfd8c7205d6e0
davi <martinsoliveiradavi83@gmail.com>
2024-11-30 12:17:57 -0300

    corrigindo rotas

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,17 +1,17 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import Homepage from '../pages/home/homepage.jsx'
+// import { HashRouter, Route, Routes } from "react-router-dom";
+// import Homepage from '../pages/home/homepage.jsx'
 
-const App = () => {
-  return (
-    <HashRouter >
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<Homepage />} />
-        <Route path="*" element={<NotFoundError />} />
-      </Routes>
-    </HashRouter>
-  );
-};
+// const App = () => {
+//   return (
+//     <HashRouter >
+//       <Routes>
+//         <Route path='/' element={<Login />} />
+//         <Route path="/register" element={<RegisterPage />} />
+//         <Route path="/home" element={<Homepage />} />
+//         <Route path="*" element={<NotFoundError />} />
+//       </Routes>
+//     </HashRouter>
+//   );
+// };
 
-export default App;
+// export default App;
